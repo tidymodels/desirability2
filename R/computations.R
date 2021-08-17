f34e97a1d97fb91672b57e5eee2bc6a61ac0ef71
Max Kuhn <mxkuhn@gmail.com>
2021-08-17 10:49:49 -0400

    add more methods

--- R/computations.R
+++ R/computations.R
@@ -1,22 +1,72 @@
 .comp_max <- function(x, low, high, scale, missing) {
   # TODO check for numeric
   # TODO check range for missing
-  out <- x * NA
-  out[x < low] <- 0
-  out[x > high] <- 1
-  out[x <= high & x >= low] <- ((x[x <= high & x >= low] - low)/ (high - low))^scale
-  out[is.na(out)] <- missing
+  out <- rep(missing, length(x))
+  out[x < low & !is.na(x)] <- 0
+  out[x > high & !is.na(x)] <- 1
+  middle <- x <= high & x >= low & !is.na(x)
+  out[middle] <- ((x[middle] - low)/ (high - low))^scale
   out
 }
 
 .comp_min <- function(x, low, high, scale, missing) {
   # TODO check for numeric
   # TODO check range for missing
-  out <- x * NA
-  out[x < low] <- 1
-  out[x > high] <- 0
-  out[x <= high & x >= low] <- ((x[x <= high & x >= low] - high)/ (low - high))^scale
-  out[is.na(out)] <- missing
+  out <- rep(missing, length(x))
+  out[x < low & !is.na(x)] <- 1
+  out[x > high & !is.na(x)] <- 0
+  middle <- x <= high & x >= low & !is.na(x)
+  out[middle] <- ((x[middle] - high)/ (low - high))^scale
   out
 }
 
+
+.comp_target <- function(x, low, target, high, scale_low, scale_high, missing) {
+  out <- rep(missing, length(x))
+
+  out[(x < low | x > high) &  !is.na(x)] <- 0
+  lower <- x <= target & x >= low & !is.na(x)
+  out[lower] <- ((x[lower] - low) / (target - low))^scale_low
+  upper <- x <= high & x >= target & !is.na(x)
+  out[upper] <- ((x[upper] - high) / (target - high))^scale_high
+
+  out
+}
+
+
+.comp_custom <- function(x, values, d, missing) {
+  ord <- order(values)
+  values <- values[ord]
+  d <- d[ord]
+
+  out <- rep(missing, length(x))
+  out[x < min(values) & !is.na(x)] <- 0
+  out[x > max(values) & !is.na(x)] <- 1
+
+  middle <- x < max(values) & x > min(values) & !is.na(x)
+  x_mid <- x[middle]
+  out[middle] <- stats::approx(values, d, xout = x_mid)$y
+
+  out
+}
+
+
+.comp_box <- function(x, low, high, missing) {
+  out <- rep(missing, length(x))
+  out[x < low | x > high & !is.na(x)] <- 0
+  out[x >= low & x <= high & !is.na(x)] <- 1
+
+  out
+}
+
+
+.comp_category <- function(x, values, missing) {
+
+  # check value names and values, make consisten factors when needed
+
+  values <- tibble::tibble(value = names(values), d = unname(values))
+  dat <- tibble::tibble(value = x, order = seq_along(x))
+  out <- dplyr::left_join(dat, values, by = "value")
+
+  out$d[out$order]
+}
--- R/in-line.R
+++ R/in-line.R
@@ -11,11 +11,16 @@
 #' (1980) are implemented.
 #'
 #' @param x A vector of data to compute the desirability function
-#' @param low,high Single numeric values that define the active ranges of
+#' @param low,high,target Single numeric values that define the active ranges of
 #' desirability.
-#' @param scale A single numeric value to rescale the desirability function.
+#' @param scale,scale_low,scale_high A single numeric value to rescale the
+#' desirability function.
 #' @param missing A single numeric value on `[0, 1]` (or `NA_real_`) that
 #' defines how missing values in `x` are mapped to the desirability score.
+#' @param x_vals,desirability Numeric vectors of the same length that define the
+#' desirability results at specific values of `x`.
+#' @param categories A named list of desirability values that match all
+#' possible categories to specific desirability values.
 #' @return A numeric vector on `[0, 1]` where larger values are more
 #' desirable.
 #' @seealso [d_max_select()], [d_overall()]
@@ -28,8 +33,8 @@
 #' library(ggplot2)
 #'
 #' set.seed(1)
-#' dat <- tibble(x = sort(runif(10)), y = sort(runif(10)))
-#' d_max(dat$x, 0.1, 0.75)
+#' dat <- tibble(x = sort(runif(30)), y = sort(runif(30)))
+#' d_max(dat$x[1:10], 0.1, 0.75)
 #'
 #' dat %>%
 #'   mutate(d_x = d_max(x, 0.1, 0.75))
@@ -48,12 +53,94 @@
 #'   lims(x = 0:1, y = 0:1) +
 #'   coord_fixed() +
 #'   ylab("Desirability")
-d_max <- function(x, low, high, scale = 1, missing = 1) {
+#'
+#' # ------------------------------------------------------------------------------
+#' # Target example
+#'
+#' dat %>%
+#'   mutate(
+#'     triangle = d_target(x, 0.1, 0.5, 0.9, scale_low = 2, scale_high = 1/2)
+#'   ) %>%
+#'   ggplot(aes(x = x, y = triangle)) +
+#'   geom_point() +
+#'   geom_line(alpha = .5) +
+#'   lims(x = 0:1, y = 0:1) +
+#'   coord_fixed() +
+#'   ylab("Desirability")
+#'
+#' # ------------------------------------------------------------------------------
+#' # Box constraints
+#'
+#' dat %>%
+#'   mutate(box = d_box(x, 1/4, 3/4)) %>%
+#'   ggplot(aes(x = x, y = box)) +
+#'   geom_point() +
+#'   geom_line(alpha = .5) +
+#'   lims(x = 0:1, y = 0:1) +
+#'   coord_fixed() +
+#'   ylab("Desirability")
+#'
+#' # ------------------------------------------------------------------------------
+#' # Custom function
+#'
+#' v_x <- seq(0, 1, length.out = 20)
+#' v_d <- 1 - exp(-10 * abs(v_x - .5))
+#'
+#' dat %>%
+#'   mutate(v = d_custom(x, v_x, v_d)) %>%
+#'   ggplot(aes(x = x, y = v)) +
+#'   geom_point() +
+#'   geom_line(alpha = .5) +
+#'   lims(x = 0:1, y = 0:1) +
+#'   coord_fixed() +
+#'   ylab("Desirability")
+#'
+#' # ------------------------------------------------------------------------------
+#' # Qualitative data
+#'
+#' set.seed(3)
+#' groups <- sort(runif(10))
+#' names(groups) <- letters[1:10]
+#'
+#' tibble(x = letters[1:7]) %>%
+#'   mutate(d = d_category(x, groups)) %>%
+#'   ggplot(aes(x = x, y = d)) +
+#'   geom_bar(stat = "identity") +
+#'   lims(y = 0:1) +
+#'   ylab("Desirability")
+d_max <- function(x, low, high, scale = 1, missing = NA_real_) {
   .comp_max(x, low, high, scale, missing)
 }
 
 #' @rdname inline_desirability
 #' @export
-d_min <- function(x, low, high, scale = 1, missing = 1) {
+d_min <- function(x, low, high, scale = 1, missing = NA_real_) {
   .comp_min(x, low, high, scale, missing)
+}
+
+
+#' @rdname inline_desirability
+#' @export
+d_target <- function(x, low, target, high, scale_low = 1, scale_high = 1, missing = NA_real_) {
+  .comp_target(x, low, target, high, scale_low, scale_high, missing)
+}
+
+
+#' @rdname inline_desirability
+#' @export
+d_box <- function(x, low, high, missing = NA_real_) {
+  .comp_box(x, low, high, missing)
+}
+
+#' @rdname inline_desirability
+#' @export
+d_custom <- function(x, x_vals, desirability, missing = NA_real_) {
+  .comp_custom(x, x_vals, desirability, missing)
+}
+
+
+#' @rdname inline_desirability
+#' @export
+d_category <- function(x, categories, missing = NA_real_) {
+  .comp_category(x, categories, missing)
 }
