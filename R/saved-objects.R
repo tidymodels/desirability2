95e6cd034651edd83249eb039ae53a57ef68b656
Max Kuhn <mxkuhn@gmail.com>
2021-08-31 20:29:44 -0400

    removed unneeded function

--- R/saved-objects.R
+++
@@ -1,9 +0,0 @@
-d_max_define <- function(.data, ..., low, high, scale = 1, missing = 1) {
-  loc <- tidyselect::eval_select(rlang::expr(c(...)), .data, strict = TRUE, allow_rename = FALSE)
-  vars <- names(.data)[loc]
-  structure(
-    list(vars = vars, low = low, high = high, scale = scale, missing = missing),
-    class = c("d_max", "desirability")
-  )
-}
-
