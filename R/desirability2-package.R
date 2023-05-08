5ca46e3abf80eb13ffdd51a9e40174a0b8ca5e63
Max Kuhn <mxkuhn@gmail.com>
2023-05-08 14:13:53 -0400

    package docs and description update

---
+++ R/desirability2-package.R
@@ -0,0 +1,16 @@
+#' In-line desirability functions
+#'
+#' Desirability functions (Derringer and Suich, 1980,
+#' <doi:10.1080/00224065.1980.11980968>) can be used for multivariate
+#' optimization.
+#'
+#' See:
+#'
+#'  - [d_max()] for individual desirability functions.
+#'  - [d_overall()] for combining functions into an overall objective function.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
