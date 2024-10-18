a2574ed6aef794ac03061100ad1a58abb0293547
topepo <mxkuhn@gmail.com>
2024-10-18 09:08:43 -0400

    move tests around

--- tests/testthat/test-in-line.R
+++ tests/testthat/test-in-line.R
@@ -129,22 +129,6 @@ test_that('correct values', {
     c(0.2, 0.4, 0.6)
   )
 
-  # ------------------------------------------------------------------------------
-
-  expect_equal(
-    res %>% mutate(d_all  = d_overall(across(starts_with("d_")))) %>% purrr::pluck("d_all"),
-    sqrt(res$d_feat * res$d_roc)
-  )
-  expect_equal(
-    res %>% mutate(d_all  = d_overall(d_feat, d_roc)) %>% purrr::pluck("d_all"),
-    sqrt(res$d_feat * res$d_roc)
-  )
-  expect_snapshot(
-    res %>% dplyr::mutate(d_all  = d_overall(across(everything()))),
-    error = TRUE
-  )
-
-
 })
 
 test_that('missing values', {
